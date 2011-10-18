438befd7063814a4d79f0d212e8f5b8cfe4cef33
Mike Plavsky <Mike.Plavsky@quest.com>
2011-10-18 18:20:55 +0000

    spot runner reads config too

--- spot_runner.rb
+++ spot_runner.rb
@@ -1,17 +1,23 @@
 #!/usr/bin/ruby
 
-require "right_aws"
-require './core.rb'
+path = File.join File.dirname( __FILE__ )
+$LOAD_PATH.push path
 
-run do |ec2, image_id, sec_group|
+require "right_aws"
+require 'core'
+
+load_cfg
+config_logger
+
+run do |ec2, image_id|
 
   spot = ec2.request_spot_instances
     
     :image_id => image_id, 
     :spot_price => 1,
-    :instance_type => "m1.small", 
-    :key_name => "webserver",
-    :groups => [sec_group]
+    :instance_type => @cfg['instance-type'], 
+    :key_name => @cfg['key-name'],
+    :groups => [@cfg['group-id']]
 
   id = wait_for_spot_instance ec2, spot[0][:spot_instance_request_id] 
 
